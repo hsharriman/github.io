c98b5c64f4f3684fe3263408dde85c137971f554
Hwei-Shin Harriman <hwei.shin.harriman@gmail.com>
2022-09-30 02:33:44 -0700

    refactored titleheaders, renamed edgeworth file

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+//GLOBALS
+let NUM_SPARKLE_LETTERS = 0;
+
+getNumSparkle = () => {
+  return NUM_SPARKLE_LETTERS;
+};
+
+// CONSTS
+const SPARKLE_TEXT = [
+  { str: "HWEI-SHIN", containerId: "landing-name" },
+  { str: "HARR-IMAN", containerId: "landing-name" },
+  { str: "Software Engineer & Designer", containerId: "landing-subtitle" },
+  { str: "ABOUT", dashes: 1, containerId: "about-title" },
+  { str: "PUBLICATIONS", dashes: 1, containerId: "publications-title" },
+  { str: "PROJECTS", dashes: 1, containerId: "projects-title" },
+];
+
+const COLORS = ["blue", "mint", "purple", "red", "pink"];
+
+// ON LOAD FUNCTIONS
 function onload(txtPath) {
   loadText(txtPath);
   setGalleryHeight();
@@ -6,6 +26,7 @@ function onload(txtPath) {
 }
 
 function onloadHome() {
+  NUM_SPARKLE_LETTERS = createSparkleLetters();
   new CircleManager();
   fadeIn();
 }
@@ -15,6 +36,7 @@ function onloadTextOnly(txtPath) {
   fadeIn();
 }
 
+// ON LOAD HELPERS
 function fadeIn() {
   let i = 0;
   for (let ele of document.getElementsByClassName("fade")) {
@@ -23,6 +45,53 @@ function fadeIn() {
     ele.style.opacity = 1;
     i++;
   }
+}
+
+function createSparkleLetters() {
+  let idCount = 0;
+  SPARKLE_TEXT.map((obj, objInd) => {
+    const container = document.getElementById(obj.containerId);
+
+    obj.str.split("").map((char) => {
+      if (char === " ") {
+        let spanEle = document.createElement("span");
+        spanEle.innerHTML = "&nbsp;";
+        container.appendChild(spanEle);
+      } else {
+        //additional linebreak spans
+        if (idCount === 4 || idCount === 9 || idCount === 14) {
+          container.appendChild(document.createElement("br"));
+        }
+        if (idCount === 35 || idCount === 55 || idCount === 66) {
+          let lineBreak = document.createElement("span");
+          lineBreak.className = "secondlinebreak";
+          lineBreak.appendChild(document.createElement("br"));
+          container.appendChild(lineBreak);
+        }
+        let letter = createLetterSpan(char, idCount);
+        container.appendChild(letter);
+        idCount++;
+      }
+    });
+
+    // add dashes if necessary
+    if (obj.dashes) {
+      for (let i = 0; i < obj.dashes; i++) {
+        let mdash = createLetterSpan("_", idCount);
+        container.appendChild(mdash);
+        idCount++;
+      }
+    }
+  });
+  return idCount;
+}
+
+function createLetterSpan(innerHTML, idCount) {
+  let letter = document.createElement("span");
+  letter.innerHTML = innerHTML;
+  letter.className = randomColor();
+  letter.id = idCount.toString();
+  return letter;
 }
 
 class Circle {
@@ -62,29 +131,16 @@ class CircleManager {
       [20, 40, "red"], // research
     ];
     this.circles = circle_info.map((arr, i) => {
-      const [right, down, color] = arr;
-      return new Circle(right, down, color, i);
+      const [left, top, color] = arr;
+      return new Circle(left, top, color, i);
     });
   }
 }
 
-function sparkle() {
-  let colors = [
-    "var(--blue)",
-    "var(--mint)",
-    "var(--purple)",
-    "var(--red)",
-    "var(--pink)",
-  ];
-  let index = Math.floor(Math.random() * 65);
-  let letter = document.getElementById(index.toString());
-  let ogColor = letter.style.color;
-  let sparkleColor = colors[Math.floor(Math.random() * 5)];
-
-  letter.animate([{ color: sparkleColor }, { color: ogColor }], {
-    duration: 3000,
-    easing: "ease-in",
-  });
+function loadText(md) {
+  let converter = new showdown.Converter();
+  let html = converter.makeHtml(md);
+  document.getElementById("txt-container").innerHTML = html;
 }
 
 function setGalleryHeight() {
@@ -98,11 +154,19 @@ function setGalleryHeight() {
   }
 }
 
-function loadText(md) {
-  let converter = new showdown.Converter();
-  let html = converter.makeHtml(md);
-  document.getElementById("txt-container").innerHTML = html;
-}
+const sparkle = () => {
+  let index = Math.floor(Math.random() * NUM_SPARKLE_LETTERS);
+  let letter = document.getElementById(index.toString());
+  let ogColor = letter.style.color;
+
+  letter.animate([{ color: `var(--${randomColor()})` }, { color: ogColor }], {
+    duration: 3000,
+    easing: "ease-in",
+  });
+};
+
+// UTILITY FUNCTIONS
+const randomColor = () => COLORS[Math.floor(Math.random() * 5)];
 
 document.onreadystatechange = function () {
   if (document.readyState !== "complete") {
